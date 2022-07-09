b3d660ce5a5d18bb01c93b31b43cb1159e6fa9e7
Yui-Arthur <arthur80633@gmail.com>
2022-07-09 23:23:52 +0800

    test_2

--- Asm_Project/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-else:
-                #     r+=subImage.getpixel((j-avg,i+ii))[0]
-                #     g+=subImage.getpixel((j-avg,i+ii))[1]
-                #     b+=subImage.getpixel((j-avg,i+ii))[2]
